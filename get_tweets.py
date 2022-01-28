00138a404f8292068eb1ec9230f051cf4361223c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-01-28 17:19:45 -0500

    refactor to write to file on every tweet so even if its interrupted its still written

--- get_tweets.py
+++ get_tweets.py
@@ -4,19 +4,33 @@ from alive_progress import alive_bar
 
 with open("twitterids") as ids:
     ids = ids.read().strip("\n").split("\n")
+try:
+    with open("finished") as finished_ids:
+        finished = json.load(finished_ids)
+except IOError:
+    finished = []
 
-ourjson = []
+def writejson(finished, ourjson):
+    to_write = ""
+    for tweet in ourjson:
+        to_write += f"{tweet}\n"
+    with open("tweets.jsonl", "w+") as fil:
+        fil.write(to_write)
+    with open("finished", "w+") as fin:
+        fin.write(json.dumps(finished))
 
 with alive_bar(total=len(ids)) as bar:
     for identifier in ids:
+        if int(identifier) in finished:
+            print(f"{identifier} already in archive file; skipping")
+            bar()
+            continue
+        ourjson = []
         bar.text(identifier)
         scraper = TwitterTweetScraper(identifier, mode=TwitterTweetScraperMode.RECURSE)
         for tweet in scraper.get_items():
-            ourjson.append(tweet.json())
+            tweetdata = tweet.json()
+            finished.append(tweet.id)
+            ourjson.append(tweetdata)
+        writejson(finished, ourjson)
         bar()
-
-jso = ""
-for js in ourjson:
-    jso += f"{js}\n"
-with open("final", "w+") as f:
-    f.write(jso)
